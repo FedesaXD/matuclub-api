3ea6d8788ce0be85800a5019125bc878f6032b98
FedesaXD <143647903+FedesaXD@users.noreply.github.com>
2026-04-08 21:22:11 -0300

    FILTROS EN TOPS 2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 import psycopg2.extras
-from fastapi import FastAPI, HTTPException, Header
+from fastapi import FastAPI, HTTPException, Header, Request
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from dotenv import load_dotenv
